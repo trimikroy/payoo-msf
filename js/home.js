46c07cb5f2e5098692285a6c256e4c8e43b418aa
trimikpro <vupotiroy112@gmail.com>
2025-07-07 12:35:04 +0600

    add money new balance

--- js/home.js
+++ js/home.js
@@ -17,6 +17,32 @@ document.getElementById('btn-add-money')
         const addMoneyInput = document.getElementById('input-add-money').value;
        const pinNumberInput = document.getElementById('input-pin-number').value;
        // console.log(addMoneyInput, pinNumberInput);
+        
+       //step-3 verify pin number 
+       //wrong way to validate pin 
+       if(pinNumberInput === '1234'){
+        console.log('add money to the account')
+
+        // step-4 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
        
+        //step-5 add addMoneyInput with balance
+
+        const addMoneyBalance = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+
+        const newBalance = addMoneyBalance + balanceNumber;
+        console.log(newBalance);
+
+        // step-6 update the balance in the ui
+        document.getElementById('account-balance').innerText = newBalance;
+
+
+        // cash out button click to current balance to cash out 
+        
+
+       }else{
+        alert('field to add money. Please try again')
+       }
       
     });
